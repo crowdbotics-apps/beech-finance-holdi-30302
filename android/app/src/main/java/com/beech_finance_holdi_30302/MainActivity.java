f12c5cfdffa6b26654411228501b1333edd423c3
Crowdbotics <team@crowdbotics.com>
2021-09-03 22:42:52 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/beech_finance_holdi_30302/MainActivity.java
@@ -0,0 +1,15 @@
+package com.beech_finance_holdi_30302;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "beech_finance_holdi_30302";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "beech_finance_holdi_30302Identifier",
+  url: "https://beech_finance_holdi_30302Identifier.botics.co",
+  api: "https://beech_finance_holdi_30302Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
